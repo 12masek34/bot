7eb1921a87c2af56fca890a9b5bab129e6a4feae
12masek34 <12masek34@mail.ru>
2022-02-05 23:53:44 +0300

    поправил гет дата

--- main.py
+++ main.py
@@ -132,7 +132,8 @@ class BotAPI(MethodView):
                 else:
                     rsp = get_data_from_api(temporary_message[0])
                     if rsp:
-                        send_message(chat_id, rsp['title'] + '\n' + rsp['text'], rsp)
+                        message = rsp['title'] + '\n' + '\n' + rsp['text']
+                        send_message(chat_id, message, rsp)
             else:
                 send_message(chat_id, text)
         except KeyError:
